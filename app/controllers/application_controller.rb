7266fa0b77205507b82a4bce5c213f1e1ea1a68a
Dima Rogov <ds.rogv@gmail.com>
2015-04-08 00:39:39 +0300

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  
+  def hello
+  	render text: "Hello, this is my first rails app! Further changes will be made in a few weeks."
+  end
+end
