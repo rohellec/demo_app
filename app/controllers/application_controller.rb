1233340a3b3a6f4af9fb0cea246563fb3296ef60
Dima Rogov <ds.rogv@gmail.com>
2015-05-01 00:38:44 +0300

    Create User resource using scaffolding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
-  def hello
-  	render text: "Hello, this is my first rails app! Further changes will be made in a few weeks."
-  end
-
-  def hello_non_ascii
-  	render text: "\u00A1Hello, this is my first rails app! Further changes will be made in a few weeks."
-  end
+    
 end
