34292834e44f7f4be6b9d00469813b55b544ccae
Dima Rogov <ds.rogv@gmail.com>
2015-06-05 22:29:50 +0300

    Update Micropost model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
+  validates :user_id,
+            presence: true
   validates :content,
             length: { maximum: 140 },
             presence: true
